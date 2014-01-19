727e501449ad0a5a3db92c1ddc92c6f9f81cec11
Daniel Keller <dbk40@case.edu>
2014-01-19 11:37:01 -0500

    factored out window code

--- AS1/main.cpp
+++ AS1/main.cpp
@@ -1,25 +1,11 @@
+#include "Shader.hpp"
+#include "Object.hpp"
+#include "Window.hpp"
+
 #include "GL/gl_core_3_3.h"
 #include "GLFW/glfw3.h"
 
-#include "Shader.hpp"
-#include "Object.hpp"
-
 #include <iostream>
-
-void CheckGLError()
-{
-	GLenum err = glGetError();
-	while (err != GL_NO_ERROR) {
-		std::cerr << "Gl error [" << err << "] " << (err) << std::endl; //gluErrorString
-		err = glGetError();
-	}
-}
-
-static void error_callback(int error, const char* description)
-{
-    fputs(description, stderr);
-	getchar();
-}
 
 static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
@@ -28,65 +14,46 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
 }
 
 int main(void)
+try
 {
-    GLFWwindow* window;
-    glfwSetErrorCallback(error_callback);
-    if (!glfwInit()) 
-        exit(EXIT_FAILURE);
-	
-	//only allow non-deprecated GL3.3 calls
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	
-    window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        exit(EXIT_FAILURE);
-    }
+	//create the basic window
+	Window w;
+	//add our keyboard input callback
+    glfwSetKeyCallback(w.window, key_callback);
 
-    glfwMakeContextCurrent(window);
-	//glfwSwapInterval(1); //set vsync
-    glfwSetKeyCallback(window, key_callback);
-	/*
-	glewExperimental = GL_TRUE; //needed for core profile to load correctly
-	GLenum err = glewInit();
-	glGetError(); //clear bogus GL error
-	if (err != GLEW_OK)
-	{
-		fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
-        exit(EXIT_FAILURE);
-	}*/
-	if(ogl_LoadFunctions() == ogl_LOAD_FAILED)
-	{
-		std::cerr << "Error in glLoadGen\n";
-		glfwDestroyWindow(window);
-		glfwTerminate();
-        exit(EXIT_FAILURE);
-	}
-
+	//load the shader
 	ShaderProgram prgm("simple.vert", "simple.frag");
+
+	//load the object
 	Object obj;
+
+	//clear to black
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
     
-	while (!glfwWindowShouldClose(window))
+	//our main loop
+	while (!glfwWindowShouldClose(w.window))
     {
-        float ratio;
+		//set the GL draw surface to the same size as the window
         int width, height;
-        glfwGetFramebufferSize(window, &width, &height);
-        ratio = width / (float) height;
+        glfwGetFramebufferSize(w.window, &width, &height);
 		glViewport(0, 0, (GLsizei) width, (GLsizei) height);
 
-		glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+		//clear the color buffer
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		//draw the object
 		prgm.use();
 		obj.draw();
 
-        glfwSwapBuffers(window);
+		//swap draw buffer and visible buffer
+        glfwSwapBuffers(w.window);
         glfwPollEvents();
     }
-    glfwDestroyWindow(window);
-    glfwTerminate();
-    exit(EXIT_SUCCESS);
+    
+	return EXIT_SUCCESS;
+}
+catch (const char* mesg)
+{
+	std::cerr << mesg << "\nExiting...\n";
+	return EXIT_FAILURE;
 }
